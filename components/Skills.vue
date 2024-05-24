705ed7068b38a70fa725170f8a0fe78211f42470
Vadim <asasin020@mail.ru>
2024-05-24 17:59:39 +0200

    Changed skills section and add the footer

---
+++ components/Skills.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <div class="mb-7">
+            <div class="flex justify-between py-1">
+                <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Web Design</span>
+                <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">50%</span>
+            </div>
+            <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                <path class="rc-progress-line-trail" d="M 0.5,0.5
+             L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                <path class="rc-progress-line-path" d="M 0.5,0.5
+             L 99.5,0.5" stroke-linecap="round" stroke="#CD5C5C" stroke-width="1" fill-opacity="0"
+                    style="stroke-dasharray: 50.2px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                </path>
+            </svg>
+        </div>
+
+        <div class="mb-7">
+            <div class="flex justify-between py-1">
+                <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Développement d'application</span>
+                <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">60%</span>
+            </div>
+            <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                <path class="rc-progress-line-trail" d="M 0.5,0.5
+             L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                <path class="rc-progress-line-path" d="M 0.5,0.5
+             L 99.5,0.5" stroke-linecap="round" stroke="#F08080" stroke-width="1" fill-opacity="0"
+                    style="stroke-dasharray: 60.05px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                </path>
+            </svg>
+        </div>
+
+        <div class="mb-7">
+            <div class="flex justify-between py-1">
+                <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Server</span>
+                <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">60%</span>
+            </div>
+            <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                <path class="rc-progress-line-trail" d="M 0.5,0.5
+             L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                <path class="rc-progress-line-path" d="M 0.5,0.5
+             L 99.5,0.5" stroke-linecap="round" stroke="#FA8072" stroke-width="1" fill-opacity="0"
+                    style="stroke-dasharray: 60.35px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                </path>
+            </svg>
+        </div>
+
+        <div class="mb-7">
+            <div class="flex justify-between py-1">
+                <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Photoshop</span>
+                <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">55%</span>
+            </div>
+            <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                <path class="rc-progress-line-trail" d="M 0.5,0.5
+             L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                <path class="rc-progress-line-path" d="M 0.5,0.5
+             L 99.5,0.5" stroke-linecap="round" stroke="#E9967A" stroke-width="1" fill-opacity="0"
+                    style="stroke-dasharray: 50.25px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                </path>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+ 
+};
+</script>
+
+<style>
+.rc-progress-line {
+  width: 100%;
+  height: 6px;
+}
+</style>
